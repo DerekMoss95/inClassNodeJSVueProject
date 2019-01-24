ce112ea497a987be4f0bac9185575c0cb1d5b39a
DerekMoss95 <mossderek88@gmail.com>
2019-01-23 18:38:21 -0700

    Initialized the project and made many changes to the routing, default views, created login page etc

---
+++ src/views/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="login">
+    <h1>This is a login page</h1>
+    <form @submit.prevent="login">
+      Username: <input type ="text" v-model="username"><br>
+      Password: <input type ="password" v-model="password"><br>
+      <input type ="submit" value="Login">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data: () => {
+    return {
+      username: "",
+      password: ""
+    }
+  },
+  methods: {
+    login() {
+      window.user = {
+        username: this.username,
+        password: this.password
+      }
+      this.$router.push('home')
+    }
+  }
+}
+</script>
